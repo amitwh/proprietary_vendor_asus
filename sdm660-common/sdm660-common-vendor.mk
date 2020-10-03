4700e75536593288f950e0a06f70882496ef449e
Bruno Martins <bgcngm@gmail.com>
2020-10-03 18:49:52 +0530

    sdm660-common: Add MIUI offmode charging daemon

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -559,7 +559,8 @@ PRODUCT_PACKAGES += \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
-    WfdCommon
+    WfdCommon \
+    chargeonlymode
 
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
 PRODUCT_COPY_FILES += \
